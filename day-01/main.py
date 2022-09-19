75e4ef7aae1818e099fc4758d58244c094a433df
Romeo Enso <renso@NBMRENSO.local>
2022-09-19 19:12:41 +1000

    day-01 getting input, variable and print result

---
+++ day-01/main.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Generator.")
+
+city = input("Whats city you grow up with?\n")
+
+pet_name = input("What is the name of your pet?\n")
+
+print("You're band name is " + city + " " + pet_name)
