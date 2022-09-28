56646d990ed8181f3e0ecaf5c4307db1851a28a5
Romeo Enso <romeo.enso93@gmail.com>
2022-09-28 22:52:14 +1000

    DS: linear search

--- day-12/linear_search.py
+++ day-12/linear_search.py
@@ -5,7 +5,20 @@ def linear_search(list, target):
     for i in range(0, len(list)):
         if list[i] == target:
             return i
-    return "None"
+    return None
 
-print(linear_search([1,2,3,4,5], 5))
-print(linear_search([1,2,3,4,5], 10))
+def verify(index):
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in list")
+
+numbers = [x for x in range(0, 11)]
+result = linear_search(numbers, 6)
+verify(result)
+
+result = linear_search(numbers, 10)
+verify(result)
+
+result = linear_search(numbers, 12)
+verify(result)
