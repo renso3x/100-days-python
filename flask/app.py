ddc2dbbed84dfae0395144b838a513de04d76068
Romeo Enso <romeo.enso93@gmail.com>
2022-10-05 18:52:04 +1100

    http method GET/POST, retrieve form data

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,20 @@
-from distutils.debug import DEBUG
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", content=['Romeo', "Joy", "Bob"])
+    return render_template("index.html") 
 
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["name"]
+        return redirect(url_for("user", name=user))
+    else:
+        return render_template("login.html")
+    
 # Accept url parameters in the link
 @app.route('/<name>')
 def user(name):
@@ -19,4 +27,4 @@ def admin():
     return redirect(url_for("user", name="Admin!"))
 
 if __name__ == "__main__":
-    app.run(DEBUG=True)
+    app.run(debug=True)
