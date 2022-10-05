d1a1385afc392f2b79fc79180f4049a311d9429d
Romeo Enso <romeo.enso93@gmail.com>
2022-10-05 20:48:28 +1100

    Flask integrating sql alchemy

--- flask/Readme.md
+++ flask/Readme.md
@@ -7,3 +7,12 @@
 ## Running the app
 
 > FLASK_APP=[NAME].py flask run
+
+# SQL Alchemy
+
+> Setup https://www.digitalocean.com/community/tutorials/how-to-use-flask-sqlalchemy-to-interact-with-databases-in-a-flask-application
+>
+> > > export FLASK_APP=app
+> > > flask shell
+> > > from app import db, Student
+> > > db.create_all()
--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,30 @@
 from datetime import timedelta
-from re import I
+import os
 from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.secret_key = "somesecret"
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+#DB
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'users.sqlite3')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+db.init_app(app)
+# Model
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
 
 @app.route("/")
 def home():
@@ -14,6 +34,19 @@ def home():
 def login():
     if request.method == "POST":
         user = request.form["name"]
+        flash(f"Login successful.")
+
+        # Match if the user exist
+        found_user = users.query.filter_by(name=user).first() 
+
+        if found_user:
+            session["email"] = found_user.email
+            session["user_id"] = found_user._id
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+
         if user == 'admin':
             session["admin"] = user
             return redirect(url_for("admin"))
@@ -24,11 +57,30 @@ def login():
         return render_template("login.html")
     
 # Accept url parameters in the link
-@app.route('/<name>')
+@app.route('/<name>', methods=["POST", "GET"])
 def user(name):
+    email = None
+    user_id = None
     if "user" in session:
-        flash(f"Login successful.")
-        return render_template("user.html", user=name)
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = users.query.filter_by(name=name).first() 
+            found_user.email = email
+            session["user"] = found_user.name
+            session["user_id"] = found_user._id
+
+            email = session["email"]
+            user_id = session["user_id"]
+            db.session.commit()
+            flash("Email was saved!")
+        else:
+            if "email" in session and "user_id" in session:
+                email = session["email"]
+                user_id = session["user_id"]
+                
+        return render_template("user.html", user=name, email=email, user_id=user_id)
+        
     return redirect(url_for("login"))
 
 @app.route('/admin')
@@ -38,6 +90,12 @@ def admin():
     elif "user" in session:
         return redirect(url_for("user", name=session["user"]))
     return redirect(url_for("login"))
+
+@app.route('/user/<id>')
+def view(id):
+    if "user_id" in session:
+        user = users.query.filter_by(_id=id).first()
+        return f"User: {user.name}. Email: {user.email}"
 
 @app.route('/logout')
 def logout():
