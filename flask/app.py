cbe67c0ca5913b4d3db648b85ea34241cb7361c5
Romeo Enso <romeo.enso93@gmail.com>
2022-10-05 19:09:52 +1100

    add expire session time

--- flask/app.py
+++ flask/app.py
@@ -1,19 +1,23 @@
+from datetime import timedelta
 from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
 app.secret_key = "somesecret"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
-    return render_template("index.html") 
+    return redirect(url_for("login"))
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         user = request.form["name"]
-        session["user"] = user
         if user == 'admin':
+            session["admin"] = user
             return redirect(url_for("admin"))
+
+        session["user"] = user
         return redirect(url_for("user", name=user))
     else:
         return render_template("login.html")
@@ -28,15 +32,16 @@ def user(name):
 
 @app.route('/admin')
 def admin():
-    if "user" in session:
+    if "admin" in session:
        return f"<h1>Hello Admin</h1> <a href='/logout' >Logout</a>" 
-    #Redirect url to a function name
-    # Pass url param when redirecting
+    elif "user" in session:
+        return redirect(url_for("user", name=session["user"]))
     return redirect(url_for("login"))
 
 @app.route('/logout')
 def logout():
-    session.pop("user", None)
+    for key in list(session.keys()):
+        session.pop(key)
     return redirect(url_for("login"))
 
 
