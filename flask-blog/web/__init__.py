6e023b533c07459e9851d5b94088c01f3e8d3518
Romeo Enso <romeo.enso93@gmail.com>
2022-10-11 14:01:44 +1100

    Added login and signup form

--- flask-blog/web/__init__.py
+++ flask-blog/web/__init__.py
@@ -11,6 +11,6 @@ def create_app():
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/auth")
+    app.register_blueprint(auth, url_prefix="/")
 
     return app
--- flask-blog/web/views.py
+++ flask-blog/web/views.py
@@ -3,7 +3,5 @@ from flask import Blueprint, render_template
 views = Blueprint("views", __name__)
 
 @views.route('/')
-@views.route("/<name>")
-def home(name):
-    print(name)
-    return render_template("home.html", name=name)
+def home():
+    return render_template("home.html")
