2ccc5b4e464bb756abc8b3f1bc85e20ded295ef7
Romeo Enso <romeo.enso93@gmail.com>
2022-10-11 13:36:43 +1100

    init flask, add home view

---
+++ flask-blog/readme.md
@@ -0,0 +1,7 @@
+## Depedencies
+
+- pip install flask Flask-SQLAlchemy flask-login
+
+## Run Application
+
+- FLASK_APP=app.py flask run
---
+++ flask-blog/web/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "helloworld"
+
+    @app.route("/")
+    def home():
+        return "<h1>HEllo</h1>"
+
+    @app.route("/profile")
+    def profile():
+        return "<h1>Profile</h1>"
+
+    return app
