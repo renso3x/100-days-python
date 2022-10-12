80bfd283de30aefa18ec4c85f666d77186b08f76
Romeo Enso <romeo.enso93@gmail.com>
2022-10-12 12:16:47 +1100

    setup SQLAlchemy, applied authentication via flask_login

--- flask-blog/web/__init__.py
+++ flask-blog/web/__init__.py
@@ -3,14 +3,42 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
 
+    # DB Configuration
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    # API Routes here
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    # Import all the models before creating the DB
+    from .models import User
+        
+    create_database(app)
+
+    # Restrict user to login page
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    # @login_manager.user_loader - returns a session
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+def create_database(app):
+    with app.app_context():
+        db.create_all()
+        print("Create database!")
